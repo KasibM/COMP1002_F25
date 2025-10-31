03487cac3db466b01b894688a5483417342aa6d3
KasibM <syuriix@yahoo.ca>
2025-10-30 22:41:56 -0400

    removing "[]"s

--- assignment-1/order.php
+++ assignment-1/order.php
@@ -18,12 +18,12 @@
     $user_phone = $_GET["phone"];
 
     $pizza_quantity = $_GET["quantity"];
-    $pizza_style = $_GET["pizza_style[]"];
-    $pizza_size = $_GET["pizza_size[]"];
-    $pizza_shape = $_GET["pizza_shape[]"];
-    $pizza_toppings = $_GET["pizza_toppings[]"];
+    $pizza_style = $_GET["pizza_style"];
+    $pizza_size = $_GET["pizza_size"];
+    $pizza_shape = $_GET["pizza_shape"];
+    $pizza_toppings = $_GET["pizza_toppings"];
 
-    $delivery_method = $_GET["delivery_method[]"];
+    $delivery_method = $_GET["delivery_method"];
     $instructions = $_GET["instructions"];
 
     echo('<p>Thank you '.$user_name.', your ');
